699a471b8237c75dc62c599b54976f9720eb6c87
Ruben Paredes <ruben.pardes25@gmail.com>
2022-04-09 18:26:42 -0400

    Hero logo animation (2/?) follow mouse animation

--- src/components/Hero/HeroLogoAnimation.tsx
+++ src/components/Hero/HeroLogoAnimation.tsx
@@ -7,35 +7,60 @@ const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.heading};
   margin-left: 2em;
   display: flex;
-  
+
   align-items: center;
   justify-content: center;
 `;
 
-const Dot = styled.div`
+const Dot = styled.div<{ dy: number; dx: number }>`
   background-color: black;
   width: 10px;
   height: 10px;
 
-  transition: all 1s linear;
-`
+  transition: all 2s linear;
+`;
 
 const HeroLogoAnimation = () => {
-  const dotRef = React.useRef<HTMLDivElement>(null)
-  const [trans, setTrans] = useState([0, 0])
+  const dotRef = React.useRef<HTMLDivElement>(null);
+  const [mousePos, setMousePos] = useState([0, 0]);
+  const [trans, setTrans] = useState([0, 0]);
+  const [mouseMoveTimeout, setMouseMoveTimeout] = useState<NodeJS.Timeout>();
 
-  const handleMouseEnterContainer: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    console.log(`[Container] pageX: ${e.pageX}`)
-    console.log(`[Container] pageY: ${e.pageY}`)
-    console.log(`[Container] dotRef.current?.offsetLeft:`, dotRef.current?.offsetLeft)
-    console.log(`[Container] dotRef.current?.offsetTop:`, dotRef.current?.offsetTop)
-  }
+  const handleMouseMoveContainer: React.MouseEventHandler<HTMLDivElement> = (
+    e
+  ) => {
+    // console.log(`[Mouse] pageX: ${e.pageX}`)
+    // console.log(`[Mouse] pageY: ${e.pageY}`)
+    // console.log(`[Dot] dotRef.current?.offsetLeft:`, dotRef.current?.offsetLeft)
+    // console.log(`[Dot] dotRef.current?.offsetTop:`, dotRef.current?.offsetTop)
 
-  const handleMouseLeaveContainer: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    return 0
-  }
+    mouseMoveTimeout && clearTimeout(mouseMoveTimeout);
+    const _mouseMoveTimeout = setTimeout(() => {
+      setTrans([0, 0]);
+      setMousePos([0, 0]);
+    }, 20);
+    setMouseMoveTimeout(_mouseMoveTimeout);
 
-  return <Container onMouseEnter={handleMouseEnterContainer} onMouseLeave={handleMouseLeaveContainer}><Dot ref={dotRef} style={{translate: }}/></Container>;
+    if (mousePos[0] && mousePos[1]) {
+      const dMouseX = e.pageX - mousePos[0];
+      const dMouseY = e.pageY - mousePos[1];
+      setTrans((prev) => [prev[0] + dMouseX * 4, prev[1] + dMouseY * 4]);
+      // console.log(`dMouseX: ${dMouseX}, dMouseY: ${dMouseY}`)
+    }
+
+    setMousePos([e.pageX, e.pageY]);
+  };
+
+  return (
+    <Container onMouseMove={handleMouseMoveContainer}>
+      <Dot
+        ref={dotRef}
+        style={{ transform: `translate(${trans[0]}px, ${trans[1]}px)` }}
+        dy={trans[1]}
+        dx={trans[0]}
+      />
+    </Container>
+  );
 };
 
 export default HeroLogoAnimation;
